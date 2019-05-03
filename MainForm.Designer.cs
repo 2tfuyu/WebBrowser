07b53bc732db4f70b77e84fdec69869c1d9ce02f
2tf <39296516+nitf@users.noreply.github.com>
2019-05-03 12:48:43 +0900

    WinFormの学習

---
+++ MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WebBrowser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.URL = new System.Windows.Forms.TextBox();
+            this.load = new System.Windows.Forms.Button();
+            this.WebBrowser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // URL
+            // 
+            this.URL.Location = new System.Drawing.Point(110, 12);
+            this.URL.Name = "URL";
+            this.URL.Size = new System.Drawing.Size(1008, 25);
+            this.URL.TabIndex = 0;
+            // 
+            // load
+            // 
+            this.load.Location = new System.Drawing.Point(12, 8);
+            this.load.Name = "load";
+            this.load.Size = new System.Drawing.Size(92, 32);
+            this.load.TabIndex = 1;
+            this.load.Text = "Load";
+            this.load.UseVisualStyleBackColor = true;
+            this.load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // WebBrowser
+            // 
+            this.WebBrowser.Location = new System.Drawing.Point(1, 46);
+            this.WebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowser.Name = "WebBrowser";
+            this.WebBrowser.Size = new System.Drawing.Size(1270, 602);
+            this.WebBrowser.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1283, 641);
+            this.Controls.Add(this.WebBrowser);
+            this.Controls.Add(this.load);
+            this.Controls.Add(this.URL);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox URL;
+        private System.Windows.Forms.Button load;
+        private System.Windows.Forms.WebBrowser WebBrowser;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebBrowser
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            this.SizeChanged += new EventHandler(this.ReSized);
+            WebBrowser.Navigate("https://www.google.co.jp");
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            WebBrowser.Navigate(URL.Text);
+        }
+
+        private void ReSized(object sender, EventArgs e)
+        {
+            int width = this.ClientSize.Width;
+            int height = this.ClientSize.Height;
+            WebBrowser.Size = new Size(width, height - 30);
+        }
+        
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.ClientSize = new Size(1200, 830);
+        }
+    }
+}
